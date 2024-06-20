1679527beade6e8dd9e26aee84c9f0f379697901
hueychen27 <hueychen27@outlook.com>
2024-06-19 22:37:23 -0700

    Better code

--- Eating-food.py
+++ Eating-food.py
@@ -1,12 +1,13 @@
 Eaten_food = list()
 while True:
-    food = input('What do you want to eat?')
-    if food == 'stop':
-        break
-    elif food == 'Cordonyellow' or food == 'cordonyellow':
-        print('Do not eat the creator of this program!')
-    elif food == 'Github' or food == 'github':
-		print('You don\'t like Github? :(')
-    else:
-        Eaten_food.append(food)
-    print('You have eaten', Eaten_food)
+    food = input('What do you want to eat?').lower()
+    match food:
+        case 'stop':
+            break
+        case 'cordonyellow':
+            print('Do not eat the creator of this program!')
+        case 'github':
+            print('You don\'t like Github? :(')
+        case _:
+            Eaten_food.append(food)
+    print('You have so far eaten', Eaten_food)
